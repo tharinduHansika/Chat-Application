610398615b355fb4ae4310291e91dd838344660f
tharinduHansika <tharindunirma@gmail.com>
2022-08-15 22:50:45 +0530

    Server Controller Restored

--- src/controller/ServerController.java
+++ src/controller/ServerController.java
@@ -1,54 +1,93 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
 import com.sun.security.ntlm.NTLMException;
 import com.sun.security.ntlm.Server;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Timer;
 
 public class ServerController {
-    private ServerSocket serverSocket;
+    @FXML
+    public JFXTextField clientName;
 
-    public ServerController(ServerSocket serverSocket){
-        this.serverSocket = serverSocket;
-    }
+    Stage stage;
 
-    public void startServer(){
-        try{
-            while (!serverSocket.isClosed()){
+    Server server;
+    Thread mainThread;
 
-                Socket socket = serverSocket.accept();
+    Socket localSocket;
+    public static HashMap<Integer, DataOutputStream> clients = new HashMap<>();
 
-                /*Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setContentText("New Client Connected");*/
-                System.out.println("New Client Connected");
+    public void initialize() throws IOException {
 
-                ClientHandler clientHandler = new ClientHandler(socket);
+        // open up the server
+        server = new Server(8080, 5);
 
-                Thread thread = new Thread(clientHandler);
-                thread.start();
-
+        mainThread = new Thread(() -> {
+            // always waits for a client's request
+            while (true) {
+                try {
+                    localSocket = server.accept();
+                    Timer timer = new Timer();
+                    timer.schedule(new Flusher(new DataInputStream(localSocket.getInputStream()),timer),0,2000);
+                    clients.put(localSocket.getPort(), new DataOutputStream(localSocket.getOutputStream()));
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
             }
-        } catch (IOException e){
+        });
 
-        }
+        mainThread.start();
+
+        Platform.runLater(() -> {
+            stage.setOnCloseRequest(e -> {
+                mainThread.interrupt();
+                // TODO : have to check why jvm is not exiting from above code block
+                System.exit(0);
+            });
+        });
     }
 
-    public void closeServerSocket(){
+    public void openUpClient(ActionEvent actionEvent) throws IOException {
+
+        // opening chat-area for client
+        Stage clientStage = new Stage(StageStyle.DECORATED);
+        FXMLLoader loader = new FXMLLoader(this.getClass().getClassLoader().getResource("com/chatwithme/FXML/client.fxml"));
+        Scene client = new Scene(loader.load());
+
+        clientStage.setScene(client);
+        clientStage.setTitle("User : " + clientName.getText());
+        clientStage.setResizable(false);
+        clientStage.sizeToScene();
+
+        // passing data via the controller
         try {
-            if (serverSocket != null){
-                serverSocket.close();
-            }
-        }catch (IOException e){
-            e.printStackTrace();
+            clientController controller = loader.getController();
+            controller.initData(clientName.getText(),clientStage);
+        }catch (NullPointerException e){
+            System.out.println(e.getMessage());
         }
+
+        clientStage.show();
+        clientName.clear();
+
     }
 
-    public static void main(String args[]) throws IOException{
-        ServerSocket serverSocket = new ServerSocket(1234);
-        ServerController serverController = new ServerController (serverSocket);
-        serverController.startServer();
+    public void initData (Stage stage) {
+        this.stage = stage;
     }
 }
