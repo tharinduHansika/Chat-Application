06b510d6e23bc970241d076d874c906f0de8bb6b
tharinduHansika <tharindunirma@gmail.com>
2022-08-15 22:29:59 +0530

    client util created.

---
+++ src/util/Client.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client extends Socket {
+
+    String name;
+    DataOutputStream out;
+
+    public Client (String name, String host, int port) throws IOException {
+        super(host,port);
+        this.name = name;
+        out = new DataOutputStream(this.getOutputStream());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DataOutputStream getOut() {
+        return out;
+    }
+
+    public void setOut(DataOutputStream out) {
+        this.out = out;
+    }
+}
